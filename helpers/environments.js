51e70bc06bf7027ebb5a0a822fb452a3061811bb
naime-ahmed <naime2molla@gmail.com>
2024-05-19 23:01:46 +0600

    file to store all the environment variables

---
+++ helpers/environments.js
@@ -0,0 +1,23 @@
+// module scaffolding
+const environments = {};
+
+environments.staging = {
+    port: 3000,
+    envName: 'staging',
+};
+
+environments.production = {
+    port: 5000,
+    ennName: 'production',
+};
+
+// determine which environment was passed
+const currentEnvironment =    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+
+// export corresponding environment object
+const environmentsToExport =    typeof environments[currentEnvironment] === 'object'
+        ? environments[currentEnvironment]
+        : environments.staging;
+
+// export module
+module.exports = environmentsToExport;
