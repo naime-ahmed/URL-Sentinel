6937bc8201668ea430c475a1604628ca89877847
naime-ahmed <naime2molla@gmail.com>
2024-05-19 23:04:29 +0600

    pushed choseHandler function into end even t lister for easy data access

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -30,24 +30,21 @@ handler.handleReqRes = (req, res) => {
     let realData = '';
 
     const choseHandler = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
-
-    choseHandler(requestProperties, (statusCode, payload) => {
-        statusCode = typeof statusCode === 'number' ? statusCode : 500;
-        payload = typeof payload === 'object' ? payload : {};
-
-        const payloadString = JSON.stringify(payload);
-
-        // return the final response
-        res.writeHead(statusCode);
-        res.end(payloadString);
-    });
-
     req.on('data', (buffer) => {
         realData += decoder.write(buffer);
     });
     req.on('end', () => {
         realData += decoder.end();
-        console.log(realData);
+        choseHandler(requestProperties, (statusCode, payload) => {
+            statusCode = typeof statusCode === 'number' ? statusCode : 500;
+            payload = typeof payload === 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            // return the final response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+        });
         // response handle
         res.end('Hello folks');
     });
