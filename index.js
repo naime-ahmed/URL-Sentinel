e6a8589500d2fe44bb973e3ac8c4853167f2805e
naime-ahmed <naime2molla@gmail.com>
2024-05-19 23:08:30 +0600

    accessing port from environment file and testing file system function of data file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Title: Uptime Monitoring Application
+Title: Raw Node.js Project - URL-Sentinel
 Description: A REASTFul API to monitor up or down time of user defined links
 Author: Naime Molla
 Date: 18/05/24
@@ -8,20 +8,22 @@ Date: 18/05/24
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object = module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
+// testing file system
+data.delete('test', 'newFile', (err) => {
+    console.log(err);
+});
 
 // Create server
 app.createServer = () => {
     const server = http.createServer(handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`Listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`Listening to port ${environment.port}`);
     });
 };
 
