ba2c3f2015196056ae9c8a4a8e7ba832cfb1e4b2
naime-ahmed <naime2molla@gmail.com>
2024-05-23 01:38:52 +0600

    added new route called user

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 // dependencies
 const { sampleHandlers } = require('./handlers/routeHandlers/sampleHandlers');
+const { userHandlers } = require('./handlers/routeHandlers/userHandler');
 
 const routes = {
     sample: sampleHandlers,
+    user: userHandlers,
 };
 
 module.exports = routes;
