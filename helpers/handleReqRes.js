dfdeb980e91f795379346b8e7918997ad11ee9c7
naime-ahmed <naime2molla@gmail.com>
2024-05-23 01:37:10 +0600

    added realData into requestProperties in json formate

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -3,6 +3,7 @@ const url = require('url');
 const { StringDecoder } = require('string_decoder');
 const routes = require('../routes');
 const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
+const { parseJSON } = require('./utilities');
 
 // module scaffolding
 const handler = {};
@@ -35,6 +36,9 @@ handler.handleReqRes = (req, res) => {
     });
     req.on('end', () => {
         realData += decoder.end();
+
+        requestProperties.body = parseJSON(realData);
+
         choseHandler(requestProperties, (statusCode, payload) => {
             statusCode = typeof statusCode === 'number' ? statusCode : 500;
             payload = typeof payload === 'object' ? payload : {};
@@ -42,11 +46,10 @@ handler.handleReqRes = (req, res) => {
             const payloadString = JSON.stringify(payload);
 
             // return the final response
+            res.setHeader('Content-Type', 'application/json');
             res.writeHead(statusCode);
             res.end(payloadString);
         });
-        // response handle
-        res.end('Hello folks');
     });
 };
 module.exports = handler;
