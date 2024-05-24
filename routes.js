4ed826a70ac8ccdbad8d8de60d3481f38e665f86
naime-ahmed <naime2molla@gmail.com>
2024-05-25 01:34:15 +0600

    added new route called tokec

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 // dependencies
 const { sampleHandlers } = require('./handlers/routeHandlers/sampleHandlers');
 const { userHandlers } = require('./handlers/routeHandlers/userHandler');
+const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler');
 
 const routes = {
     sample: sampleHandlers,
     user: userHandlers,
+    token: tokenHandler,
 };
 
 module.exports = routes;
