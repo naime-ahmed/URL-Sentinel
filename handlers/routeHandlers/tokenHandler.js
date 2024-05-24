5fac7a390a0e49a6c6126119bb55b4e244021386
naime-ahmed <naime2molla@gmail.com>
2024-05-25 02:12:01 +0600

    post method has built successfully

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -1,5 +1,7 @@
 // dependency
 const data = require('../../lib/data');
+const { hash, createRandomString } = require('../../helpers/utilities');
+const { parseJSON } = require('../../helpers/utilities');
 
 // Module scaffolding
 const handler = {};
@@ -16,7 +18,45 @@ handler.tokenHandler = (requestProperties, callback) => {
 
 handler._token = {};
 
-handler._token.post = (requestProperties, callback) => {};
+handler._token.post = (requestProperties, callback) => {
+    let { phone, password } = requestProperties.body;
+    phone = typeof phone === 'string' && phone.trim().length === 11 ? phone.trim() : false;
+    password = typeof password === 'string' && password.trim().length > 0 ? password.trim() : false;
+
+    if (phone && password) {
+        data.read('users', phone, (err1, userData) => {
+            const hashedPassword = hash(password);
+            if (hashedPassword === parseJSON(userData).password) {
+                const tokenId = createRandomString(20);
+                const expires = Date.now() + 60 * 60 * 1000;
+                const tokenObject = {
+                    phone,
+                    id: tokenId,
+                    expires,
+                };
+
+                // store the token
+                data.create('tokens', tokenId, tokenObject, (err2) => {
+                    if (!err2) {
+                        callback(200, tokenObject);
+                    } else {
+                        callback(500, {
+                            error: 'there was a problem in the server side',
+                        });
+                    }
+                });
+            } else {
+                callback(400, {
+                    error: 'password is not correct',
+                });
+            }
+        });
+    } else {
+        callback(400, {
+            error: 'you have a problem in your request',
+        });
+    }
+};
 // @TODO Authentication
 handler._token.get = (requestProperties, callback) => {};
 // @TODO Authentication
