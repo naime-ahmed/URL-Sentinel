809926ed03ce5d9fec024c212cecff54ec26a99f
naime-ahmed <naime2molla@gmail.com>
2024-06-08 00:30:55 +0600

    added new routes called checkHandler for uptime url checking

--- routes.js
+++ routes.js
@@ -2,11 +2,13 @@
 const { sampleHandlers } = require('./handlers/routeHandlers/sampleHandlers');
 const { userHandlers } = require('./handlers/routeHandlers/userHandler');
 const { tokenHandler } = require('./handlers/routeHandlers/tokenHandler');
+const { checkHandler } = require('./handlers/routeHandlers/checkHandler');
 
 const routes = {
     sample: sampleHandlers,
     user: userHandlers,
     token: tokenHandler,
+    check: checkHandler,
 };
 
 module.exports = routes;
